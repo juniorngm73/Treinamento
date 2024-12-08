a638a22dccdf608d6d5d32952e38c6de9d75f9bf
juniorngm73 <juniorngm@yahoo.com.br>
2024-12-07 22:50:43 -0300

    ff

--- ex013.py
+++
@@ -1,8 +0,0 @@
-# Aumento de salario
-
-salario = float(input('Digite o valor do salário:'))
-aumento = float(input('Digite o Percentual de aumento:'))/100
-
-novo_salario = salario + (salario * aumento)
-print(f'Você obteve aumento de {aumento*100}%, Seu Salário agora é R$ {novo_salario} reais')
-
