7a57b94104d9fc532530f0f71eba5b97dfe54945
Natanael Gomes de Melo Junior <143910888+juniorngm73@users.noreply.github.com>
2025-01-26 23:47:08 -0300

    Update ex012.py

--- python/ex012.py
+++ python/ex012.py
@@ -4,3 +4,4 @@ desc = float(input('Digite o valor do desconto:'))/100
 vdesc = valor * desc
 vfinal = valor - vdesc
 print(f'Seu desconto foi de {vdesc} reais , Você Pagará {vfinal} reais')
+teste do python
